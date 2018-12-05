eb07b652e3360fcd93971e2e39ea735ad7925942
abhiilash <abhiparmar87@gmail.com>
2018-12-05 10:52:56 +0530

    add error message

--- src/component/Home/View.js
+++ src/component/Home/View.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, ActivityIndicator, ListView, Linking, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator, ListView } from 'react-native';
 import { Card, CardTitle, CardContent, CardAction, CardButton, CardImage } from 'react-native-material-cards'
 
 const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
@@ -8,8 +8,9 @@ export default class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      articles: ds.cloneWithRows([]),
-      isLoading: false
+      articles: [],
+      isLoading: false,
+      message: ''
     };
   }
 
@@ -19,14 +20,21 @@ export default class Home extends React.Component {
   }
 
   componentWillReceiveProps(nextProps){
-    if('newsData' in  nextProps && 'articles' in nextProps.newsData){
-      this.setState({isLoading: false, articles: ds.cloneWithRows(nextProps.newsData.articles)})
+    if('newsData' in  nextProps && 'status' in nextProps.newsData && nextProps.newsData.status !== "error"){
+      if('newsData' in  nextProps && 'articles' in nextProps.newsData){
+        this.setState({isLoading: false, articles: nextProps.newsData.articles})
+      }
+    }else{
+      if('newsData' in  nextProps && 'message' in nextProps.newsData){
+        this.setState({isLoading: false, message: nextProps.newsData.message})
+      }
     }
+    
   }
 
   renderNews(rowData){
     return(
-      <TouchableOpacity onPress={()=> Linking.openURL(rowData.url)}>
+      <View>
         <Card>
           <CardImage 
             source={{uri: rowData.urlToImage ? rowData.urlToImage : 'http://www.gaby-moreno.com/administrator/public_html/images/no-image.gif'}} 
@@ -38,12 +46,13 @@ export default class Home extends React.Component {
            />
            <CardContent text={rowData.content ? rowData.content : ''}/>
          </Card>
-      </TouchableOpacity>
+      </View>
     )
   }
 
   render() {
-    const {articles, isLoading} = this.state
+    const {articles, isLoading, message} = this.state
+    console.log("articles", articles)
     return (
       <View style={styles.container}>
         {isLoading ?
@@ -51,11 +60,17 @@ export default class Home extends React.Component {
             <ActivityIndicator size="large" color="#e91e63" />
             <Text style={styles.loaderText}>fetching news...</Text>
           </View>
-        :
-          <ListView
-            dataSource={articles}
-            renderRow={(rowData) => this.renderNews(rowData)}
-          />
+        : 
+          articles.length ?
+            <ListView
+              dataSource={ds.cloneWithRows(articles)}
+              renderRow={(rowData) => this.renderNews(rowData)}
+            />
+          :
+            <View style={styles.loadingContainer}>
+              <Text style={styles.loaderText}>{message}</Text>
+            </View>
+          
         }
       </View>
     );
